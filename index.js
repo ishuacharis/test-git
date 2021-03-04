9c75e0ca26452d0fa943392719e1f63008bb364a
ishuacharis <ishuacharis@gmail.com>
2021-03-05 00:18:15 +0100

    first comit

---
+++ index.js
@@ -0,0 +1,11 @@
+const courses = ['biology', 'physics', 'geography', 'law'];
+
+let allCourses = [];
+
+for (let i = 0; i < courses.length; i++) {
+    
+    allCourses.push(courses[i]);
+    
+}
+
+console.log(allCourses);
