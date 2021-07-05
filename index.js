dfa4272edfe21c6acf18b076b3122fb2ea8500cc
olawale-o <omoogunolawale@gmail.com>
2021-07-05 20:18:57 +0100

    Modify index.js File

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const courses = ['biology', 'physics', 'geography', 'law'];
 
 const allCourses = courses.map((course) => course)
 console.log(allCourses);
+
+console.log("created new ssh key on mac");
